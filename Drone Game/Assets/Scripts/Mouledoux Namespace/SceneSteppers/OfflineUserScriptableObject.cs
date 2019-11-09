0934bcfe098627cd7f51ae0da1928e20d36f4dd9
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-11-09 10:25:26 -0600

    Inittal

---
+++ Drone Game/Assets/Scripts/Mouledoux Namespace/SceneSteppers/OfflineUserScriptableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UserInfo", menuName = "ScriptableObjects/DBUserInfo")]
+public class OfflineUserScriptableObject : ScriptableObject
+{
+	[SerializeField]
+	public UserInformation userInfo;
+
+
+	public void ClearUser()
+	{
+		userInfo = new UserInformation("", -1);
+	}
+
+
+    public sealed class UserInformation
+    {
+		public string sessionId;
+		public int userID;
+
+        public UserInformation(string sid, int uid)
+        {
+			sessionId = sid;
+			userID = uid;
+        }
+    }
+}
