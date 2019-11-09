aa2571f37dd59bbecc8d113826e34e2877cca0e5
Jeremy Barzas <jbarzas@gmail.com>
2019-11-09 15:53:20 -0600

    Test scene update

---
+++ Drone Game/Assets/Scripts/DroneSignalReciever.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class DroneSignalReciever : MonoBehaviour
+{
+    [SerializeField] bool m_hasSignal;
+    [SerializeField]SignalTransmitter m_closetTransmitter;
+    [SerializeField]List<SignalTransmitter> m_inRangeTransmitters;
+
+    public bool HasSignal => (HasSignal);
+    public SignalTransmitter CloseestTransmitter => (m_closetTransmitter);
+
+    private void Awake()
+    {
+        m_inRangeTransmitters = new List<SignalTransmitter>();
+    }
+
+    private void Update()
+    {
+        if (m_inRangeTransmitters.Count <= 0)
+        {
+            m_hasSignal = false;
+            return;
+        }
+        else
+            m_hasSignal = true;
+
+        if (!m_hasSignal)
+            GetComponentInParent<TestDroneMovment>().enabled = false;
+        else
+            GetComponentInParent<TestDroneMovment>().enabled = true;
+
+        CheckSignalStrength();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("SignalArea")) return;
+        if (m_inRangeTransmitters.Count <= 0)
+        {
+            m_inRangeTransmitters.Add(other.gameObject.GetComponent<SignalTransmitter>());
+            m_closetTransmitter = other.gameObject.GetComponent<SignalTransmitter>();
+        }
+        if (!m_inRangeTransmitters.Contains(other.gameObject.GetComponent<SignalTransmitter>()))
+        {
+            m_inRangeTransmitters.Add(other.gameObject.GetComponent<SignalTransmitter>());
+        }   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("SignalArea")) return;
+        if (m_inRangeTransmitters.Count <= 0) return;
+        if (!m_inRangeTransmitters.Contains(other.gameObject.GetComponent<SignalTransmitter>()));
+            m_inRangeTransmitters.Add(other.gameObject.GetComponent<SignalTransmitter>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("SignalArea")) return;
+        if (m_inRangeTransmitters.Count <= 0) return;
+        m_inRangeTransmitters.Remove(other.gameObject.GetComponent<SignalTransmitter>());
+    }
+
+    void CheckSignalStrength()
+    {
+        if (m_inRangeTransmitters.Count <= 0) return;
+        if (m_closetTransmitter == null)
+            m_closetTransmitter = m_inRangeTransmitters[0];
+        foreach (SignalTransmitter st in m_inRangeTransmitters)
+        {
+            if (m_closetTransmitter.SignalStength > st.SignalStength)
+                m_closetTransmitter = st;
+        }
+    }
+}
---
+++ Drone Game/Assets/Scripts/TestDroneMovment.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDroneMovment : MonoBehaviour
+{
+    bool hasSignal = false;
+    public float m_moveSpeed = 1;
+    public float m_turnSpeed = 1;
+
+    private void Awake()
+    {
+        hasSignal = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputY = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(0, 0, inputY);
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 turn = new Vector3(0, inputX, 0);
+        transform.Translate(move * m_moveSpeed);
+        transform.Rotate(turn * m_turnSpeed);
+    }
+}
