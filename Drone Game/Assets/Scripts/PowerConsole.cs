761175ae7f1d42e47da7c0d67d3b799e762ba5c6
Facehead <jbarzas@gmail.com>
2019-11-10 11:58:26 -0600

    started working on power delivery

---
+++ Drone Game/Assets/Scripts/PowerConsole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerConsole : MonoBehaviour
+{
+    [SerializeField] List<SignalTransmitter> m_signalTowers;
+    [SerializeField] List<PowerChipSocket> m_powerChipSockets;
+
+    private void Awake()
+    {
+        m_signalTowers = new List<SignalTransmitter>();
+        m_powerChipSockets = new List<PowerChipSocket>();
+    }
+}
